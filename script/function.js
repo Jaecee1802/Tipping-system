8873c7edda45458afc1e94c6397605c5916ce974
JC Banzuela <162450651+Jaecee1802@users.noreply.github.com>
2024-11-29 15:43:30 +0800

    Add files via upload

--- script/function.js
+++ script/function.js
@@ -3,7 +3,9 @@ const tip = document.getElementById("tipAmount");
 const addtipBill = document.getElementById("add-tip-bill");
 const totalBill = document.getElementById("total");
 
+
 function calcTotal(){
+    event.preventDefault();
     const billVal = bill.value;
     const tipVal = tip.value;
     const totalVal = billVal * (1 + tipVal / 100);
