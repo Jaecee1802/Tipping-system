277a1b14dea70e8b1135621b49fe2a2e9fd3a951
JC Banzuela <162450651+Jaecee1802@users.noreply.github.com>
2024-11-29 15:36:02 +0800

    Add files via upload

---
+++ script/function.js
@@ -0,0 +1,13 @@
+const bill = document.getElementById("billAmount");
+const tip = document.getElementById("tipAmount");
+const addtipBill = document.getElementById("add-tip-bill");
+const totalBill = document.getElementById("total");
+
+function calcTotal(){
+    const billVal = bill.value;
+    const tipVal = tip.value;
+    const totalVal = billVal * (1 + tipVal / 100);
+    totalBill.innerText = totalVal.toFixed(2);
+}
+
+addtipBill.addEventListener("click", calcTotal);
